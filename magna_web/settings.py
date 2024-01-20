f80a279ccd13588199b072a09a5222becaa182ad
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2024-01-20 10:57:12 -0500

    Update settings.py

--- magna_web/settings.py
+++ magna_web/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-3gc^^v-mqu9hjtgz148b)5q2+b9%ng8#j2f62mr=-75@uayss$' #os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") 
-DOMAIN=os.environ.get('DOMAIN_DEV') if DEBUG else os.environ.get('DOMAIN_PROD')
+DEBUG = True #os.environ.get("DEBUG") 
+DOMAIN= 'localhost:8000/auth/users'# os.environ.get('DOMAIN_DEV') if DEBUG else os.environ.get('DOMAIN_PROD')
 
 ALLOWED_HOSTS = ["*"]  #env.list("ALLOWED_HOSTS_DEV") if DEBUG else env.list("ALLOWED_HOSTS_PROD")
 
