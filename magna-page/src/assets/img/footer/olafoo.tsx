7c534818ba4ab2a4f4f10ee97985c385b3e70a96
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2024-02-05 00:00:19 -0500

    quitando peso

--- magna-page/src/assets/img/footer/olafoo.tsx
+++
@@ -1,8 +0,0 @@
-
-
-export const OlaFooter = ({fill="#800000",fillOpa="1"}) => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill={fill} fill-opacity={fillOpa} d="M0,96L80,96C160,96,320,96,480,112C640,128,800,160,960,181.3C1120,203,1280,213,1360,218.7L1440,224L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-
-    )
-}
