bf644fa926fdb50488ac2e045b08c9279b75c411
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2024-02-04 23:34:12 -0500

    arranque

--- magna-page/src/apiClient.tsx
+++ magna-page/src/apiClient.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
+// const APIURL = 'http://127.0.0.1:8000/'
+const APIURL ="http://54.211.37.134:8000/"
 
 const apiClient = axios.create({
-  baseURL:'http://127.0.0.1:8000/' ,
+  baseURL:APIURL ,
   headers: {
     'Content-type': 'application/json',
   },
--- magna-page/src/constans.tsx
+++ magna-page/src/constans.tsx
@@ -1,13 +1,4 @@
 
-export const API_URL='http://127.0.0.1:8000'
-export const API_EQUIPOS='http://127.0.0.1:8000/equipos/'
-export const API_SERVICIOS='http://127.0.0.1:8000/servicios/'
-export const API_PROYECTOS='http://127.0.0.1:8000/proyectos/'
-export const API_PREGUNTASYRESPUESTAS='http://127.0.0.1:8000/frequentQuestions/'
-export const API_CONTACTO='http://127.0.0.1:8000/contact/'
-// export const API_URL='http://54.211.37.134:8000'
-// export const API_EQUIPOS='http://54.211.37.134:8000/equipos/'
-// export const API_SERVICIOS='http://54.211.37.134:8000/servicios/'
-// export const API_PROYECTOS='http://54.211.37.134:8000/proyectos/'
-// export const API_PREGUNTASYRESPUESTAS='http://54.211.37.134:8000/frequentQuestions/'
-// export const API_CONTACTO='http://54.211.37.134:8000/contact/'
+// export const API_URL='http://127.0.0.1:8000'
+
+export const API_URL='http://54.211.37.134:8000'
