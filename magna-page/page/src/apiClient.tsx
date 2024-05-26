6c65922d7fb9669b78935c7a4ded769548416cd5
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2024-05-26 10:20:12 -0500

    new server

--- magna-page/page/src/apiClient.tsx
+++ magna-page/page/src/apiClient.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 // export const APIURL = 'http://127.0.0.1:8000/'
-export const APIURL ="http://54.211.37.134:8000/"
+// export const APIURL ="http://54.211.37.134:8000/"
+export const APIURL = 'https://rn1nb289-8000.use2.devtunnels.ms/'
 
 const apiClient = axios.create({
   baseURL:APIURL ,
