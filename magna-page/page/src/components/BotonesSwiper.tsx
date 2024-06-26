bacfd65f99b7aa6e3a68c2a3f1304c23c021f3f6
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2024-06-26 00:03:43 -0500

    accecibilidad

--- magna-page/page/src/components/BotonesSwiper.tsx
+++ magna-page/page/src/components/BotonesSwiper.tsx
@@ -5,7 +5,7 @@ export const BotonesSwiper = () => {
   return (
     <>
         <span onClick={() => swiper.slideNext()} role='button' className="swiper-button-next custom-next-icon" aria-label="Botón para pasar a la siguiente diapositiva" ></span>
-        <span  onClick={() => swiper.slidePrev()} className="swiper-button-prev custom-prev-icon" aria-label="Botón para pasar a la diapositiva anterior"></span>
+        <span  onClick={() => swiper.slidePrev()} ><button className="swiper-button-prev custom-prev-icon" aria-label="Pasar a la diapositiva anterior" style={{backgroundColor:"transparent",border:"none"}}></button></span>
     </>
   );
 };
